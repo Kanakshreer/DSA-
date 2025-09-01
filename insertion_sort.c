7f4943eba8e9498f451f6e0b9c21356ddb670d9d
Kanakshree Rathore <rathorekanakshree@gmail.com>
2025-09-01 18:45:42 +0530

    Add files via upload

---
+++ insertion_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {99, 4, 33, 6, 25, 70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i;
+
+    insertionSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
